276e32c3cc374998a173ab5891d47e83660e83af
Shamil Yusifov <syusifov22791@ada.edu.az>
2026-02-14 12:39:49 +0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,5 @@
+Public static void main(Strig[] args){
+     setInvoice("books", "books for reading", 4, 2.0)
+     System.out.println(getInvoice());
+     System.out.println("Price of invoice " + getInvoiceAmount(5.4))
+}
